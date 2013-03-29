5d384d7760c0a06de4d8d534550f18b04716f750
Pascal Widdershoven <pascal@kabisa.nl>
2013-03-29 22:49:50 +0100

    Add /print burndown page with print styles applied by default

--- app/controllers/iterations_controller.rb
+++ app/controllers/iterations_controller.rb
@@ -3,4 +3,6 @@ class IterationsController < ApplicationController
     @burndown  = Burndown.find(params[:burndown_id]).decorate
     @iteration = @burndown.iterations.find_by_number(params[:id]).decorate
   end
+
+  alias_method :print, :show
 end
