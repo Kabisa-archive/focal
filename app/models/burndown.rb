Kabisa-archive/focal

commit 68f885b727c0ac299fda44d745f60e16b9cb3e49

Allow today's metrics to be updated

app/models/burndown.rb
@@ -23,14 +23,19 @@ class Burndown < ActiveRecord::Base
   def import
     update_burndown_utc_offset
 
-    Metric.create do |metric|
-      metric.iteration   = create_or_update_iteration
-      metric.captured_on = Time.now.utc.to_date
+    Metric.find_or_initialize_by_captured_on(Time.now.utc.to_date).tap do |metric|
+      metric.iteration = create_or_update_iteration
 
       %w(unstarted started finished delivered accepted rejected).each do |state|
         metric.send("#{state}=", pivotal_iteration.send("#{state}"))
       end
+
+      metric.save
     end
+  end
+
+  def force_update
+    import
   end
 
   # Returns the current iteration

app/admin/burndown.rb
@@ -5,6 +5,9 @@ ActiveAdmin.register Burndown do
   index do
     selectable_column
     column :name
+    column() do |burndown|
+      link_to 'Force Update', force_update_admin_burndown_path(burndown), method: :put, confirmation: "Are you sure?"
+    end
     default_actions
   end
 
@@ -26,5 +29,11 @@ ActiveAdmin.register Burndown do
     end
 
     f.actions
+  end
+
+  member_action :force_update, method: :put do
+    burndown = Burndown.find(params[:id])
+    burndown.force_update
+    redirect_to :back, notice: "Update performed. Thank you."
   end
 end

config/routes.rb
@@ -1,5 +1,6 @@
 Focal::Application.routes.draw do
   ActiveAdmin.routes(self)
+
   devise_for :admin_users, ActiveAdmin::Devise.config
 
   resources :burndowns, only: [:show] do

features/manual_refresh.feature
@@ -1,0 +1,12 @@
+Feature: Manual refresh
+    As a user
+    I can request an update of today's data
+    So the burn down reflects the current Pivotal status.
+
+    Background:
+        Given we have a fake Pivotal Tracker burndown
+        And the system imports metrics from Pivotal Tracker
+        And I am logged in as an administrator
+
+    Scenario:
+        Then I can request an update for today

features/step_definitions/pivotal_import_steps.rb
@@ -25,6 +25,17 @@ When /^the system imports metrics from Pivotal Tracker$/ do
   @expect_json = BurndownDecorator.decorate(@my_burndown).to_json
 end
 
+When /^I can request an update for today$/ do
+  burndown = double :burndown
+  burndown.should_receive(:force_update).once
+  Burndown.stub(:find).with(@my_burndown.id.to_s).and_return(burndown)
+
+  visit "/admin/burndowns"
+  within("#burndowns tr#burndown_#{@my_burndown.id}") do
+    click_link "Force Update"
+  end
+end
+
 Then /^I can see my burndown was updated$/ do
   step "I look at my burndown"
 

spec/models/burndown_spec.rb
@@ -60,6 +60,54 @@ describe Burndown do
         subject.stub(:pivotal_iteration).and_return(pivotal_double)
       end
 
+      context "force reload today" do
+        it "updates todays data" do
+          burndown.import
+
+          new_double = double :pivotal_iteration,
+            number: 123,
+            pivotal_id: 42,
+            start_at: start_datetime,
+            finish_at: finish_datetime,
+            utc_offset: 3600,
+            unstarted: 99,
+            started: 2,
+            finished: 3,
+            delivered: 5,
+            accepted: 8,
+            rejected: 13
+
+          subject.stub(:pivotal_iteration).and_return(new_double)
+
+          expect {
+            burndown.force_update
+          }.to change { Metric.last.unstarted }.from(1).to(99)
+        end
+
+        it "does not create a duplicate metric" do
+          burndown.import
+
+          new_double = double :pivotal_iteration,
+            number: 123,
+            pivotal_id: 42,
+            start_at: start_datetime,
+            finish_at: finish_datetime,
+            utc_offset: 3600,
+            unstarted: 99,
+            started: 2,
+            finished: 3,
+            delivered: 5,
+            accepted: 8,
+            rejected: 13
+
+          subject.stub(:pivotal_iteration).and_return(new_double)
+
+          expect {
+            burndown.force_update
+          }.not_to change { Metric.count }
+        end
+      end
+
       context "imports fresh data" do
         it "update burndown utc_offset" do
           expect {

commit 2e450ac9f7e878f373a9f86032831b9bf8c5440f

Fix issue with metric recording introduced in 68f885b

app/models/burndown.rb
@@ -28,17 +28,22 @@ class Burndown < ActiveRecord::Base
   def import
     update_burndown_utc_offset
 
-    Metric.find_or_initialize_by_captured_on(Time.now.utc.to_date).tap do |metric|
-      metric.iteration = create_or_update_iteration
+    metric_iteration = create_or_update_iteration
+    metric_captured_on = Time.now.utc.to_date
 
-      %w(unstarted started finished delivered accepted rejected).each do |state|
-        metric.send("#{state}=", pivotal_iteration.send("#{state}"))
-      end
+    metric = Metric.where(iteration_id: metric_iteration.id, captured_on: metric_captured_on).first ||
+      Metric.new.tap { |m|
+        m.iteration = metric_iteration
+        m.captured_on = metric_captured_on
+      }
 
-      metric.save
-
-      notify_campfire if campfire_enabled?
+    %w(unstarted started finished delivered accepted rejected).each do |state|
+      metric.send("#{state}=", pivotal_iteration.send("#{state}"))
     end
+
+    metric.save
+
+    notify_campfire if campfire_enabled?
   end
 
   def force_update